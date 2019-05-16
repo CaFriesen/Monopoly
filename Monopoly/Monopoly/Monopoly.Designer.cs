72c5acc62c879629b1a309e53553c2044b3c30f8
CaFriesen <c.friesen@student.fontys.nl>
2019-05-16 21:31:31 +0200

    Added UI elements and propertys in the classdiagram

---
+++ Monopoly/Monopoly/Monopoly.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Monopoly
+{
+    partial class Monopoly
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbUtilitiesP1 = new System.Windows.Forms.GroupBox();
+            this.btnP1profile = new System.Windows.Forms.Button();
+            this.btnP1RealEstate = new System.Windows.Forms.Button();
+            this.btnP1Upgrade = new System.Windows.Forms.Button();
+            this.lbP1Properties = new System.Windows.Forms.ListBox();
+            this.btnBuy = new System.Windows.Forms.Button();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.gbUtilitiesP2 = new System.Windows.Forms.GroupBox();
+            this.btnP2profile = new System.Windows.Forms.Button();
+            this.btnP2RealEstate = new System.Windows.Forms.Button();
+            this.btnP2Upgrade = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbP2Properties = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblP1Rent = new System.Windows.Forms.Label();
+            this.lblNamePropertyP1 = new System.Windows.Forms.Label();
+            this.lblNamePropertyP2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbUtilitiesP1.SuspendLayout();
+            this.gbUtilitiesP2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbUtilitiesP1
+            // 
+            this.gbUtilitiesP1.Controls.Add(this.lblP1Rent);
+            this.gbUtilitiesP1.Controls.Add(this.panel1);
+            this.gbUtilitiesP1.Controls.Add(this.btnP1profile);
+            this.gbUtilitiesP1.Controls.Add(this.btnP1RealEstate);
+            this.gbUtilitiesP1.Controls.Add(this.btnP1Upgrade);
+            this.gbUtilitiesP1.Controls.Add(this.lbP1Properties);
+            this.gbUtilitiesP1.Controls.Add(this.btnBuy);
+            this.gbUtilitiesP1.Controls.Add(this.btnRoll);
+            this.gbUtilitiesP1.Location = new System.Drawing.Point(11, 778);
+            this.gbUtilitiesP1.Name = "gbUtilitiesP1";
+            this.gbUtilitiesP1.Size = new System.Drawing.Size(936, 229);
+            this.gbUtilitiesP1.TabIndex = 0;
+            this.gbUtilitiesP1.TabStop = false;
+            this.gbUtilitiesP1.Text = "Utilities Player 1";
+            // 
+            // btnP1profile
+            // 
+            this.btnP1profile.BackgroundImage = global::Monopoly.Properties.Resources.playing_tokensP1;
+            this.btnP1profile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnP1profile.Location = new System.Drawing.Point(7, 129);
+            this.btnP1profile.Name = "btnP1profile";
+            this.btnP1profile.Size = new System.Drawing.Size(99, 100);
+            this.btnP1profile.TabIndex = 6;
+            this.btnP1profile.UseVisualStyleBackColor = true;
+            // 
+            // btnP1RealEstate
+            // 
+            this.btnP1RealEstate.Location = new System.Drawing.Point(768, 29);
+            this.btnP1RealEstate.Name = "btnP1RealEstate";
+            this.btnP1RealEstate.Size = new System.Drawing.Size(162, 192);
+            this.btnP1RealEstate.TabIndex = 5;
+            this.btnP1RealEstate.UseVisualStyleBackColor = true;
+            // 
+            // btnP1Upgrade
+            // 
+            this.btnP1Upgrade.Location = new System.Drawing.Point(632, 129);
+            this.btnP1Upgrade.Name = "btnP1Upgrade";
+            this.btnP1Upgrade.Size = new System.Drawing.Size(129, 94);
+            this.btnP1Upgrade.TabIndex = 3;
+            this.btnP1Upgrade.Text = "Upgrade";
+            this.btnP1Upgrade.UseVisualStyleBackColor = true;
+            // 
+            // lbP1Properties
+            // 
+            this.lbP1Properties.FormattingEnabled = true;
+            this.lbP1Properties.ItemHeight = 24;
+            this.lbP1Properties.Location = new System.Drawing.Point(236, 28);
+            this.lbP1Properties.Name = "lbP1Properties";
+            this.lbP1Properties.Size = new System.Drawing.Size(389, 196);
+            this.lbP1Properties.TabIndex = 2;
+            // 
+            // btnBuy
+            // 
+            this.btnBuy.Location = new System.Drawing.Point(631, 28);
+            this.btnBuy.Name = "btnBuy";
+            this.btnBuy.Size = new System.Drawing.Size(130, 94);
+            this.btnBuy.TabIndex = 1;
+            this.btnBuy.Text = "Buy";
+            this.btnBuy.UseVisualStyleBackColor = true;
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Location = new System.Drawing.Point(6, 28);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(224, 94);
+            this.btnRoll.TabIndex = 0;
+            this.btnRoll.Text = "Roll";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            // 
+            // gbUtilitiesP2
+            // 
+            this.gbUtilitiesP2.Controls.Add(this.panel2);
+            this.gbUtilitiesP2.Controls.Add(this.lblNamePropertyP2);
+            this.gbUtilitiesP2.Controls.Add(this.btnP2profile);
+            this.gbUtilitiesP2.Controls.Add(this.btnP2RealEstate);
+            this.gbUtilitiesP2.Controls.Add(this.btnP2Upgrade);
+            this.gbUtilitiesP2.Controls.Add(this.button1);
+            this.gbUtilitiesP2.Controls.Add(this.lbP2Properties);
+            this.gbUtilitiesP2.Controls.Add(this.button2);
+            this.gbUtilitiesP2.Location = new System.Drawing.Point(953, 778);
+            this.gbUtilitiesP2.Name = "gbUtilitiesP2";
+            this.gbUtilitiesP2.Size = new System.Drawing.Size(931, 229);
+            this.gbUtilitiesP2.TabIndex = 1;
+            this.gbUtilitiesP2.TabStop = false;
+            this.gbUtilitiesP2.Text = "Utilities Player 2";
+            // 
+            // btnP2profile
+            // 
+            this.btnP2profile.BackgroundImage = global::Monopoly.Properties.Resources.playing_tokensP2;
+            this.btnP2profile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnP2profile.Location = new System.Drawing.Point(6, 129);
+            this.btnP2profile.Name = "btnP2profile";
+            this.btnP2profile.Size = new System.Drawing.Size(99, 100);
+            this.btnP2profile.TabIndex = 7;
+            this.btnP2profile.UseVisualStyleBackColor = true;
+            // 
+            // btnP2RealEstate
+            // 
+            this.btnP2RealEstate.Location = new System.Drawing.Point(769, 24);
+            this.btnP2RealEstate.Name = "btnP2RealEstate";
+            this.btnP2RealEstate.Size = new System.Drawing.Size(162, 192);
+            this.btnP2RealEstate.TabIndex = 6;
+            this.btnP2RealEstate.UseVisualStyleBackColor = true;
+            // 
+            // btnP2Upgrade
+            // 
+            this.btnP2Upgrade.Location = new System.Drawing.Point(632, 129);
+            this.btnP2Upgrade.Name = "btnP2Upgrade";
+            this.btnP2Upgrade.Size = new System.Drawing.Size(129, 92);
+            this.btnP2Upgrade.TabIndex = 5;
+            this.btnP2Upgrade.Text = "Upgrade";
+            this.btnP2Upgrade.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(632, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 97);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Buy";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lbP2Properties
+            // 
+            this.lbP2Properties.FormattingEnabled = true;
+            this.lbP2Properties.ItemHeight = 24;
+            this.lbP2Properties.Location = new System.Drawing.Point(236, 25);
+            this.lbP2Properties.Name = "lbP2Properties";
+            this.lbP2Properties.Size = new System.Drawing.Size(389, 196);
+            this.lbP2Properties.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 94);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Roll";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Controls.Add(this.lblNamePropertyP1);
+            this.panel1.Location = new System.Drawing.Point(768, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(162, 77);
+            this.panel1.TabIndex = 2;
+            // 
+            // lblP1Rent
+            // 
+            this.lblP1Rent.AutoSize = true;
+            this.lblP1Rent.Location = new System.Drawing.Point(778, 167);
+            this.lblP1Rent.Name = "lblP1Rent";
+            this.lblP1Rent.Size = new System.Drawing.Size(34, 25);
+            this.lblP1Rent.TabIndex = 7;
+            this.lblP1Rent.Text = "$0";
+            // 
+            // lblNamePropertyP1
+            // 
+            this.lblNamePropertyP1.AutoSize = true;
+            this.lblNamePropertyP1.Location = new System.Drawing.Point(11, 31);
+            this.lblNamePropertyP1.Name = "lblNamePropertyP1";
+            this.lblNamePropertyP1.Size = new System.Drawing.Size(34, 25);
+            this.lblNamePropertyP1.TabIndex = 8;
+            this.lblNamePropertyP1.Text = "$0";
+            // 
+            // lblNamePropertyP2
+            // 
+            this.lblNamePropertyP2.AutoSize = true;
+            this.lblNamePropertyP2.Location = new System.Drawing.Point(784, 60);
+            this.lblNamePropertyP2.Name = "lblNamePropertyP2";
+            this.lblNamePropertyP2.Size = new System.Drawing.Size(34, 25);
+            this.lblNamePropertyP2.TabIndex = 9;
+            this.lblNamePropertyP2.Text = "$0";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(769, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(162, 77);
+            this.panel2.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "$0";
+            // 
+            // Monopoly
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1896, 1016);
+            this.Controls.Add(this.gbUtilitiesP2);
+            this.Controls.Add(this.gbUtilitiesP1);
+            this.Name = "Monopoly";
+            this.Text = "Monopoly";
+            this.gbUtilitiesP1.ResumeLayout(false);
+            this.gbUtilitiesP1.PerformLayout();
+            this.gbUtilitiesP2.ResumeLayout(false);
+            this.gbUtilitiesP2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbUtilitiesP1;
+        private System.Windows.Forms.Button btnP1RealEstate;
+        private System.Windows.Forms.Button btnP1Upgrade;
+        private System.Windows.Forms.ListBox lbP1Properties;
+        private System.Windows.Forms.Button btnBuy;
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.GroupBox gbUtilitiesP2;
+        private System.Windows.Forms.Button btnP2RealEstate;
+        private System.Windows.Forms.Button btnP2Upgrade;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lbP2Properties;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnP1profile;
+        private System.Windows.Forms.Button btnP2profile;
+        private System.Windows.Forms.Label lblP1Rent;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblNamePropertyP1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNamePropertyP2;
+    }
+}
+
--- Monopoly/Monopoly/Monopoly.cs
+++ Monopoly/Monopoly/Monopoly.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Monopoly
 {
-    public partial class Form1 : Form
+    public partial class Monopoly : Form
     {
-        public Form1()
+        public Monopoly()
         {
             InitializeComponent();
         }
